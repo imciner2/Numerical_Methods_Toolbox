8047c376e5d52796b822b19c0481bbe39f638781
Ian McInerney <I.McInerney17@imperial.ac.uk>
2017-11-10 18:12:06 +0000

    Empty file for cutest_getProblem

---
+++ benchmarking/cutest_getProblem.m
@@ -0,0 +1,10 @@
+function [ prob ] = cutest_getProblem( problemName, varargin )
+%CUTEST_GETPROBLEM Summary of this function goes here
+%   Detailed explanation goes here
+
+
+
+system([’cutest2matlab ’, problemName]);
+
+end
+
