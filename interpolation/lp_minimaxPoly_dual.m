2df0855de92c9b333ae0d283642ac8a1d2e4c9ee
Ian McInerney <I.McInerney17@imperial.ac.uk>
2018-02-09 18:37:38 +0000

    minimax polynomial computation

--- interpolation/lp_minimaxPoly_dual.m
+++ interpolation/lp_minimaxPoly_dual.m
@@ -1,5 +1,6 @@
-function [ coeff, E ] = lp_minimaxPoly( n, x, varargin )
-%LP_MINIMAXPOLY Compute the minimax polynomial using the primal LP
+function [ coeff, E ] = lp_minimaxPoly_dual( n, x, varargin )
+%LP_MINIMAXPOLY_DUAL Compute the minimax polynomial using the dual LP
+%formulation
 %
 % Compute the minimax polynomial approximation of order n to a discrete set
 % of points. This method uses the linear programming formulation of the
@@ -13,10 +14,10 @@ function [ coeff, E ] = lp_minimaxPoly( n, x, varargin )
 %
 %
 % Usage:
-%   [ coeff, E ] = LP_MINIMAXPOLY( n, x );
-%   [ coeff, E ] = LP_MINIMAXPOLY( n, x, poly );
-%   [ coeff, E ] = LP_MINIMAXPOLY( n, x, poly, origin );
-%   [ coeff, E ] = LP_MINIMAXPOLY( n, x, poly, origin, alg );
+%   [ coeff, E ] = LP_MINIMAXPOLY_DUAL( n, x );
+%   [ coeff, E ] = LP_MINIMAXPOLY_DUAL( n, x, poly );
+%   [ coeff, E ] = LP_MINIMAXPOLY_DUAL( n, x, poly, origin );
+%   [ coeff, E ] = LP_MINIMAXPOLY_DUAL( n, x, poly, origin, alg );
 %
 % Inputs:
 %   n      - The order of the polynomial to fit with
@@ -26,8 +27,7 @@ function [ coeff, E ] = lp_minimaxPoly( n, x, varargin )
 %            by the vandermonde function are supported here.
 %   origin - Specify an explicit value for the function at the origin. This
 %            argument is optional, by default the intercept parameter is
-%            left free for the approximation to choose. To not specify an
-%            intercept, pass in NaN.
+%            left free for the approximation to choose.
 %   alg    - Arguments to pass to the MATLAB linprog solver through
 %            optimoptions. This argument must be a cell array of the option
 %            pairs.
@@ -37,16 +37,15 @@ function [ coeff, E ] = lp_minimaxPoly( n, x, varargin )
 %   E     - The maximum approximation error over the points
 %
 %
-% see also LP_MINIMAXPOLY_DUAL
+% see also LP_MINIMAXPOLY
 %
 % Created by: Ian McInerney
 % Created on: January 30, 2018
-% Version: 1.1
+% Version: 1.0
 % Last Modified: February 9, 2018
 %
 % Revision History
 %   1.0 - Initial release
-%   1.1 - Updated problem formulation and error handling
 
 %% Parse the input
 p = inputParser;
@@ -88,38 +87,34 @@ else
     % it from the other side
     V = V(:, 2:end);
     b = x(:,2) - origin.*ones(nx, 1);
-    coeff = [1];
+    coeff = 1;
     O = zeros(1, n);
 end
 
-% Create the inequality constraint matrices (note, these are for Ax>=b)
-A_pi = [ V, e;
-        -V, e;
-         O, 1];
-b_pi = [ b;
-        -b;
-         0];
+% Create the inequality constraint matrices
+A_di = [e',  e'];
+b_di = 1;
 
- % Figure out how many coefficients there are
-[~, nf] = size(A_pi);
- 
-% Want to minimize the last variable (error)
-f = zeros(nf, 1);
-f(end) = 1;
+% Create the equality constraint matrices
+A_de = [V', -V'];
+b_de = O';
+
+% Create the cost function
+f_d = [b', -b'];
 
 
 %% Solve the linear program
 options = optimoptions('linprog', alg{:});      % Setup the options
-[c, pval, evalue] = linprog(f, -A_pi, -b_pi, [], [], [], [], options);
+[c, dval, evalue] = linprog(-f_d, A_di, b_di, A_de, b_de, zeros(length(f_d),1), [], options);
 
 
 %% Extract the coefficients and the error
 if (evalue == 1)
     coeff = [coeff;
              c(1:end-1)];
-    E = c(end);
+    E = dval;
 else
-    warning('lp_minimaxPoly:Failed to find minimax polynomial');
+    warning('lp_minimaxPoly_dual:Failed to find minimax polynomial');
     E = NaN;
 end
 
